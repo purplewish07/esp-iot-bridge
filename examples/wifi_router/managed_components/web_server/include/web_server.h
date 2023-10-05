94eaf358c4a21dea238b076d71b3ab4e5530cde1
purplewish07 <70552611+purplewish07@users.noreply.github.com>
2023-10-06 07:46:33 +0800

    update

---
+++ examples/wifi_router/managed_components/web_server/include/web_server.h
@@ -0,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <esp_err.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+esp_err_t StartWebServer(void);
+
+#ifdef __cplusplus
+}
+#endif
